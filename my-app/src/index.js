bf53892e01c050957a94dc0962637635ae89b490
SOFTWARE-GROUP\yordan.terziev <yordan.terziev@softwaregroup.com>
2018-01-30 15:57:19 +0200

    new test project

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import tasks from './reducers';
+import App from './components/App';
+import initialData from '../data';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(
+    tasks, // reducers
+    initialData // optional, just sample tasks
+);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
